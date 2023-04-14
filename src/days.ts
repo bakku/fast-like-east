db22d07b9daf8341be52af824e1361255e452a46
Christian Paling <christian.paling@googlemail.com>
2023-04-14 21:18:20 +0200

    Add fasting days for the remaining year

--- src/days.ts
+++ src/days.ts
@@ -3,199 +3,483 @@ import type { FastingDay } from "@/lib/types";
 
 export const fastingDays = [
   {
-    date: new Date(2023, 1, 22),
-    type: FastingType.STRICT,
-  },
-  {
-    date: new Date(2023, 1, 23),
-    type: FastingType.STRICT,
-  },
-  {
-    date: new Date(2023, 1, 24),
-    type: FastingType.STRICT,
-  },
-  {
-    date: new Date(2023, 1, 25),
+    date: new Date(2023, 3, 19),
     type: FastingType.WINE,
   },
   {
-    date: new Date(2023, 1, 26),
+    date: new Date(2023, 3, 21),
     type: FastingType.WINE,
   },
   {
-    date: new Date(2023, 1, 27),
-    type: FastingType.STRICT,
-  },
-  {
-    date: new Date(2023, 1, 28),
-    type: FastingType.STRICT,
-  },
-  {
-    date: new Date(2023, 2, 1),
-    type: FastingType.STRICT,
-  },
-  {
-    date: new Date(2023, 2, 2),
-    type: FastingType.STRICT,
-  },
-  {
-    date: new Date(2023, 2, 3),
-    type: FastingType.STRICT,
-  },
-  {
-    date: new Date(2023, 2, 4),
+    date: new Date(2023, 3, 26),
     type: FastingType.WINE,
   },
   {
-    date: new Date(2023, 2, 5),
+    date: new Date(2023, 3, 28),
     type: FastingType.WINE,
   },
   {
-    date: new Date(2023, 2, 6),
-    type: FastingType.STRICT,
-  },
-  {
-    date: new Date(2023, 2, 7),
-    type: FastingType.STRICT,
-  },
-  {
-    date: new Date(2023, 2, 8),
-    type: FastingType.STRICT,
-  },
-  {
-    date: new Date(2023, 2, 9),
-    type: FastingType.WINE,
-    deName: "Vierzig Märtyrer von Sebaste",
-    enName: "Forty Martyrs of Sebaste",
-  },
-  {
-    date: new Date(2023, 2, 10),
-    type: FastingType.STRICT,
-  },
-  {
-    date: new Date(2023, 2, 11),
-    type: FastingType.WINE,
-  },
-  {
-    date: new Date(2023, 2, 12),
-    type: FastingType.WINE,
-  },
-  {
-    date: new Date(2023, 2, 13),
-    type: FastingType.STRICT,
-  },
-  {
-    date: new Date(2023, 2, 14),
-    type: FastingType.STRICT,
-  },
-  {
-    date: new Date(2023, 2, 15),
-    type: FastingType.STRICT,
-  },
-  {
-    date: new Date(2023, 2, 16),
-    type: FastingType.STRICT,
-  },
-  {
-    date: new Date(2023, 2, 17),
-    type: FastingType.STRICT,
-  },
-  {
-    date: new Date(2023, 2, 18),
-    type: FastingType.WINE,
-  },
-  {
-    date: new Date(2023, 2, 19),
-    type: FastingType.WINE,
-  },
-  {
-    date: new Date(2023, 2, 20),
-    type: FastingType.STRICT,
-  },
-  {
-    date: new Date(2023, 2, 21),
-    type: FastingType.STRICT,
-  },
-  {
-    date: new Date(2023, 2, 22),
-    type: FastingType.STRICT,
-  },
-  {
-    date: new Date(2023, 2, 23),
-    type: FastingType.STRICT,
-  },
-  {
-    date: new Date(2023, 2, 24),
-    type: FastingType.STRICT,
-  },
-  {
-    date: new Date(2023, 2, 25),
+    date: new Date(2023, 4, 3),
     type: FastingType.FISH,
-    deName: "Verkündigung des Herrn",
-    enName: "Annunciation",
   },
   {
-    date: new Date(2023, 2, 26),
+    date: new Date(2023, 4, 5),
     type: FastingType.WINE,
   },
   {
-    date: new Date(2023, 2, 27),
-    type: FastingType.STRICT,
-  },
-  {
-    date: new Date(2023, 2, 28),
-    type: FastingType.STRICT,
-  },
-  {
-    date: new Date(2023, 2, 29),
-    type: FastingType.STRICT,
-  },
-  {
-    date: new Date(2023, 2, 30),
-    type: FastingType.STRICT,
-  },
-  {
-    date: new Date(2023, 2, 31),
-    type: FastingType.STRICT,
-  },
-  {
-    date: new Date(2023, 3, 1),
+    date: new Date(2023, 4, 10),
     type: FastingType.WINE,
   },
   {
-    date: new Date(2023, 3, 2),
+    date: new Date(2023, 4, 12),
+    type: FastingType.WINE,
+  },
+  {
+    date: new Date(2023, 4, 17),
     type: FastingType.FISH,
-    deName: "Palmsonntag",
-    enName: "Palm Sunday",
   },
   {
-    date: new Date(2023, 3, 3),
-    type: FastingType.STRICT,
-  },
-  {
-    date: new Date(2023, 3, 4),
-    type: FastingType.STRICT,
-  },
-  {
-    date: new Date(2023, 3, 5),
-    type: FastingType.STRICT,
-  },
-  {
-    date: new Date(2023, 3, 6),
+    date: new Date(2023, 4, 19),
     type: FastingType.WINE,
-    deName: "Gründonnerstag",
-    enName: "Holy Thursday",
   },
   {
-    date: new Date(2023, 3, 7),
-    type: FastingType.STRICT,
-    deName: "Karfreitag",
-    enName: "Holy Friday",
+    date: new Date(2023, 4, 24),
+    type: FastingType.WINE,
   },
   {
-    date: new Date(2023, 3, 8),
+    date: new Date(2023, 4, 26),
+    type: FastingType.WINE,
+  },
+  {
+    date: new Date(2023, 5, 5),
+    type: FastingType.FISH,
+  },
+  {
+    date: new Date(2023, 5, 6),
+    type: FastingType.FISH,
+  },
+  {
+    date: new Date(2023, 5, 7),
     type: FastingType.STRICT,
-    deName: "Karsamstag",
-    enName: "Holy Saturday",
+  },
+  {
+    date: new Date(2023, 5, 8),
+    type: FastingType.FISH,
+  },
+  {
+    date: new Date(2023, 5, 9),
+    type: FastingType.STRICT,
+  },
+  {
+    date: new Date(2023, 5, 10),
+    type: FastingType.FISH,
+  },
+  {
+    date: new Date(2023, 5, 11),
+    type: FastingType.FISH,
+  },
+  {
+    date: new Date(2023, 5, 12),
+    type: FastingType.FISH,
+  },
+  {
+    date: new Date(2023, 5, 13),
+    type: FastingType.FISH,
+  },
+  {
+    date: new Date(2023, 5, 14),
+    type: FastingType.STRICT,
+  },
+  {
+    date: new Date(2023, 5, 15),
+    type: FastingType.FISH,
+  },
+  {
+    date: new Date(2023, 5, 16),
+    type: FastingType.STRICT,
+  },
+  {
+    date: new Date(2023, 5, 17),
+    type: FastingType.FISH,
+  },
+  {
+    date: new Date(2023, 5, 18),
+    type: FastingType.FISH,
+  },
+  {
+    date: new Date(2023, 5, 19),
+    type: FastingType.FISH,
+  },
+  {
+    date: new Date(2023, 5, 20),
+    type: FastingType.FISH,
+  },
+  {
+    date: new Date(2023, 5, 21),
+    type: FastingType.STRICT,
+  },
+  {
+    date: new Date(2023, 5, 23),
+    type: FastingType.WINE,
+  },
+  {
+    date: new Date(2023, 5, 28),
+    type: FastingType.STRICT,
+  },
+  {
+    date: new Date(2023, 5, 30),
+    type: FastingType.STRICT,
+  },
+  {
+    date: new Date(2023, 6, 5),
+    type: FastingType.STRICT,
+  },
+  {
+    date: new Date(2023, 6, 7),
+    type: FastingType.STRICT,
+  },
+  {
+    date: new Date(2023, 6, 12),
+    type: FastingType.STRICT,
+  },
+  {
+    date: new Date(2023, 6, 14),
+    type: FastingType.STRICT,
+  },
+  {
+    date: new Date(2023, 6, 19),
+    type: FastingType.WINE,
+  },
+  {
+    date: new Date(2023, 6, 21),
+    type: FastingType.STRICT,
+  },
+  {
+    date: new Date(2023, 6, 25),
+    type: FastingType.STRICT,
+  },
+  {
+    date: new Date(2023, 6, 26),
+    type: FastingType.STRICT,
+  },
+  {
+    date: new Date(2023, 6, 27),
+    type: FastingType.STRICT,
+  },
+  {
+    date: new Date(2023, 6, 28),
+    type: FastingType.STRICT,
+  },
+  {
+    date: new Date(2023, 6, 29),
+    type: FastingType.WINE,
+  },
+  {
+    date: new Date(2023, 6, 30),
+    type: FastingType.FISH,
+  },
+  {
+    date: new Date(2023, 6, 31),
+    type: FastingType.STRICT,
+  },
+  {
+    date: new Date(2023, 7, 1),
+    type: FastingType.STRICT,
+  },
+  {
+    date: new Date(2023, 7, 2),
+    type: FastingType.STRICT,
+  },
+  {
+    date: new Date(2023, 7, 3),
+    type: FastingType.STRICT,
+  },
+  {
+    date: new Date(2023, 7, 4),
+    type: FastingType.STRICT,
+  },
+  {
+    date: new Date(2023, 7, 5),
+    type: FastingType.WINE,
+  },
+  {
+    date: new Date(2023, 7, 6),
+    type: FastingType.WINE,
+  },
+  {
+    date: new Date(2023, 7, 7),
+    type: FastingType.STRICT,
+  },
+  {
+    date: new Date(2023, 7, 9),
+    type: FastingType.STRICT,
+  },
+  {
+    date: new Date(2023, 7, 11),
+    type: FastingType.STRICT,
+  },
+  {
+    date: new Date(2023, 7, 16),
+    type: FastingType.STRICT,
+  },
+  {
+    date: new Date(2023, 7, 18),
+    type: FastingType.STRICT,
+  },
+  {
+    date: new Date(2023, 7, 22),
+    type: FastingType.STRICT,
+  },
+  {
+    date: new Date(2023, 7, 23),
+    type: FastingType.STRICT,
+  },
+  {
+    date: new Date(2023, 7, 25),
+    type: FastingType.WINE,
+  },
+  {
+    date: new Date(2023, 7, 30),
+    type: FastingType.WINE,
+  },
+  {
+    date: new Date(2023, 8, 1),
+    type: FastingType.FISH,
+  },
+  {
+    date: new Date(2023, 8, 6),
+    type: FastingType.WINE,
+  },
+  {
+    date: new Date(2023, 8, 7),
+    type: FastingType.STRICT,
+  },
+  {
+    date: new Date(2023, 8, 8),
+    type: FastingType.STRICT,
+  },
+  {
+    date: new Date(2023, 8, 13),
+    type: FastingType.WINE,
+  },
+  {
+    date: new Date(2023, 8, 15),
+    type: FastingType.STRICT,
+  },
+  {
+    date: new Date(2023, 8, 20),
+    type: FastingType.STRICT,
+  },
+  {
+    date: new Date(2023, 8, 22),
+    type: FastingType.STRICT,
+  },
+  {
+    date: new Date(2023, 8, 27),
+    type: FastingType.STRICT,
+  },
+  {
+    date: new Date(2023, 8, 29),
+    type: FastingType.WINE,
+  },
+  {
+    date: new Date(2023, 9, 4),
+    type: FastingType.STRICT,
+  },
+  {
+    date: new Date(2023, 9, 6),
+    type: FastingType.STRICT,
+  },
+  {
+    date: new Date(2023, 9, 11),
+    type: FastingType.STRICT,
+  },
+  {
+    date: new Date(2023, 9, 13),
+    type: FastingType.STRICT,
+  },
+  {
+    date: new Date(2023, 9, 18),
+    type: FastingType.STRICT,
+  },
+  {
+    date: new Date(2023, 9, 20),
+    type: FastingType.STRICT,
+  },
+  {
+    date: new Date(2023, 9, 25),
+    type: FastingType.WINE,
+  },
+  {
+    date: new Date(2023, 9, 27),
+    type: FastingType.STRICT,
+  },
+  {
+    date: new Date(2023, 10, 1),
+    type: FastingType.WINE,
+  },
+  {
+    date: new Date(2023, 10, 3),
+    type: FastingType.STRICT,
+  },
+  {
+    date: new Date(2023, 10, 8),
+    type: FastingType.STRICT,
+  },
+  {
+    date: new Date(2023, 10, 9),
+    type: FastingType.FISH,
+  },
+  {
+    date: new Date(2023, 10, 10),
+    type: FastingType.STRICT,
+  },
+  {
+    date: new Date(2023, 10, 11),
+    type: FastingType.FISH,
+  },
+  {
+    date: new Date(2023, 10, 12),
+    type: FastingType.FISH,
+  },
+  {
+    date: new Date(2023, 10, 13),
+    type: FastingType.FISH,
+  },
+  {
+    date: new Date(2023, 10, 14),
+    type: FastingType.FISH,
+  },
+  {
+    date: new Date(2023, 10, 15),
+    type: FastingType.STRICT,
+  },
+  {
+    date: new Date(2023, 10, 16),
+    type: FastingType.FISH,
+  },
+  {
+    date: new Date(2023, 10, 17),
+    type: FastingType.STRICT,
+  },
+  {
+    date: new Date(2023, 10, 18),
+    type: FastingType.FISH,
+  },
+  {
+    date: new Date(2023, 10, 19),
+    type: FastingType.FISH,
+  },
+  {
+    date: new Date(2023, 10, 20),
+    type: FastingType.FISH,
+  },
+  {
+    date: new Date(2023, 10, 21),
+    type: FastingType.FISH,
+  },
+  {
+    date: new Date(2023, 10, 22),
+    type: FastingType.STRICT,
+  },
+  {
+    date: new Date(2023, 10, 23),
+    type: FastingType.FISH,
+  },
+  {
+    date: new Date(2023, 10, 24),
+    type: FastingType.STRICT,
+  },
+  {
+    date: new Date(2023, 10, 25),
+    type: FastingType.FISH,
+  },
+  {
+    date: new Date(2023, 10, 26),
+    type: FastingType.FISH,
+  },
+  {
+    date: new Date(2023, 10, 27),
+    type: FastingType.FISH,
+  },
+  {
+    date: new Date(2023, 10, 28),
+    type: FastingType.FISH,
+  },
+  {
+    date: new Date(2023, 10, 29),
+    type: FastingType.WINE,
+  },
+  {
+    date: new Date(2023, 10, 30),
+    type: FastingType.FISH,
+  },
+  {
+    date: new Date(2023, 11, 1),
+    type: FastingType.STRICT,
+  },
+  {
+    date: new Date(2023, 11, 2),
+    type: FastingType.FISH,
+  },
+  {
+    date: new Date(2023, 11, 3),
+    type: FastingType.FISH,
+  },
+  {
+    date: new Date(2023, 11, 4),
+    type: FastingType.FISH,
+  },
+  {
+    date: new Date(2023, 11, 5),
+    type: FastingType.WINE,
+  },
+  {
+    date: new Date(2023, 11, 6),
+    type: FastingType.STRICT,
+  },
+  {
+    date: new Date(2023, 11, 7),
+    type: FastingType.STRICT,
+  },
+  {
+    date: new Date(2023, 11, 8),
+    type: FastingType.WINE,
+  },
+  {
+    date: new Date(2023, 11, 9),
+    type: FastingType.WINE,
+  },
+  {
+    date: new Date(2023, 11, 10),
+    type: FastingType.WINE,
+  },
+  {
+    date: new Date(2023, 11, 11),
+    type: FastingType.STRICT,
+  },
+  {
+    date: new Date(2023, 11, 12),
+    type: FastingType.STRICT,
+  },
+  {
+    date: new Date(2023, 11, 13),
+    type: FastingType.WINE,
+  },
+  {
+    date: new Date(2023, 11, 14),
+    type: FastingType.STRICT,
+  },
+  {
+    date: new Date(2023, 11, 15),
+    type: FastingType.STRICT,
+  },
+  {
+    date: new Date(2023, 11, 16),
+    type: FastingType.WINE,
+  },
+  {
+    date: new Date(2023, 11, 17),
+    type: FastingType.WINE,
   },
 ] as Array<FastingDay>;
