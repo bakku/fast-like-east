7714ce6389163aba159e8c141c080625cec8352f
Christian Paling <christian.paling@googlemail.com>
2025-02-26 23:17:02 +0100

    Add days for lent

--- src/days.ts
+++ src/days.ts
@@ -1,439 +1,199 @@
 import type { FastingDay } from "@/lib/types";
 import { FastingType } from "@/lib/types";
 
-export const fastingDays = [
+export const fastingDays: FastingDay[] = [
   {
-    date: new Date(2024, 5, 5),
+    date: new Date(2025, 2, 5),
+    type: FastingType.STRICT,
+    deName: "Aschermittwoch",
+    enName: "Ash Wednesday",
+  },
+  {
+    date: new Date(2025, 2, 6),
     type: FastingType.STRICT,
   },
   {
-    date: new Date(2024, 5, 7),
-    type: FastingType.WINE,
-    deName: "Heiligstes Herz Jesu",
-    enName: "Most Sacred Heart of Jesus",
-  },
-  {
-    date: new Date(2024, 5, 12),
-    type: FastingType.WINE,
-    deName: "Papst Leo III",
-    enName: "Pope Leo III",
-  },
-  {
-    date: new Date(2024, 5, 14),
+    date: new Date(2025, 2, 7),
     type: FastingType.STRICT,
   },
   {
-    date: new Date(2024, 5, 19),
-    type: FastingType.STRICT,
-  },
-  {
-    date: new Date(2024, 5, 21),
-    type: FastingType.STRICT,
-    deName: "Aloisius von Gonzaga",
-    enName: "Aloysius Gonzaga",
-  },
-  {
-    date: new Date(2024, 5, 26),
-    type: FastingType.STRICT,
-    deName: "Josemaría Escrivá",
-    enName: "Josemaría Escrivá",
-  },
-  {
-    date: new Date(2024, 5, 28),
-    type: FastingType.WINE,
-    deName: "Irenäus von Lyon",
-    enName: "Irenaeus of Lyon",
-  },
-  {
-    date: new Date(2024, 6, 3),
-    type: FastingType.WINE,
-    deName: "Apostel Thomas",
-    enName: "Thomas the Apostle",
-  },
-  {
-    date: new Date(2024, 6, 5),
-    type: FastingType.STRICT,
-    deName: "Antonio Maria Zaccaria",
-    enName: "Anthony Zaccaria",
-  },
-  {
-    date: new Date(2024, 6, 10),
-    type: FastingType.STRICT,
-  },
-  {
-    date: new Date(2024, 6, 12),
-    type: FastingType.STRICT,
-  },
-  {
-    date: new Date(2024, 6, 17),
-    type: FastingType.STRICT,
-  },
-  {
-    date: new Date(2024, 6, 19),
-    type: FastingType.STRICT,
-  },
-  {
-    date: new Date(2024, 6, 24),
-    type: FastingType.STRICT,
-  },
-  {
-    date: new Date(2024, 6, 26),
-    type: FastingType.WINE,
-    deName: "Paraskeva von Rom",
-    enName: "Paraskevi of Rome",
-  },
-  {
-    date: new Date(2024, 6, 31),
-    type: FastingType.STRICT,
-    deName: "Ignatius von Loyola",
-    enName: "Ignatius of Loyola",
-  },
-  {
-    date: new Date(2024, 7, 1),
-    type: FastingType.STRICT,
-    deName: "Heilige Makkabäer-Märtyrer",
-    enName: "Holy Maccabean Martyrs",
-  },
-  {
-    date: new Date(2024, 7, 2),
-    type: FastingType.STRICT,
-  },
-  {
-    date: new Date(2024, 7, 3),
+    date: new Date(2025, 2, 8),
     type: FastingType.WINE,
   },
   {
-    date: new Date(2024, 7, 4),
+    date: new Date(2025, 2, 9),
     type: FastingType.WINE,
   },
   {
-    date: new Date(2024, 7, 5),
+    date: new Date(2025, 2, 10),
     type: FastingType.STRICT,
   },
   {
-    date: new Date(2024, 7, 6),
-    type: FastingType.FISH,
-    deName: "Verklärung des Herrn",
-    enName: "Transfiguration of Jesus",
-  },
-  {
-    date: new Date(2024, 7, 7),
+    date: new Date(2025, 2, 11),
     type: FastingType.STRICT,
   },
   {
-    date: new Date(2024, 7, 8),
+    date: new Date(2025, 2, 12),
     type: FastingType.STRICT,
   },
   {
-    date: new Date(2024, 7, 9),
+    date: new Date(2025, 2, 13),
     type: FastingType.STRICT,
-    deName: "Edith Stein",
-    enName: "Edith Stein",
   },
   {
-    date: new Date(2024, 7, 10),
+    date: new Date(2025, 2, 14),
+    type: FastingType.STRICT,
+  },
+  {
+    date: new Date(2025, 2, 15),
     type: FastingType.WINE,
   },
   {
-    date: new Date(2024, 7, 11),
+    date: new Date(2025, 2, 16),
     type: FastingType.WINE,
   },
   {
-    date: new Date(2024, 7, 12),
+    date: new Date(2025, 2, 17),
     type: FastingType.STRICT,
   },
   {
-    date: new Date(2024, 7, 13),
+    date: new Date(2025, 2, 18),
     type: FastingType.STRICT,
   },
   {
-    date: new Date(2024, 7, 14),
+    date: new Date(2025, 2, 19),
     type: FastingType.STRICT,
-    deName: "Maximilian Kolbe",
-    enName: "Maximilian Kolbe",
+    deName: "Josefstag",
+    enName: "Saint Joseph's Day",
   },
   {
-    date: new Date(2024, 7, 16),
-    type: FastingType.STRICT,
-  },
-  {
-    date: new Date(2024, 7, 21),
+    date: new Date(2025, 2, 20),
     type: FastingType.STRICT,
   },
   {
-    date: new Date(2024, 7, 23),
+    date: new Date(2025, 2, 21),
     type: FastingType.STRICT,
   },
   {
-    date: new Date(2024, 7, 28),
-    type: FastingType.STRICT,
-  },
-  {
-    date: new Date(2024, 7, 29),
-    type: FastingType.STRICT,
-    deName: "Enthauptung Johannes' des Täufers",
-    enName: "Beheading of John the Baptist",
-  },
-  {
-    date: new Date(2024, 7, 30),
-    type: FastingType.STRICT,
-  },
-  {
-    date: new Date(2024, 8, 4),
-    type: FastingType.STRICT,
-  },
-  {
-    date: new Date(2024, 8, 6),
-    type: FastingType.STRICT,
-  },
-  {
-    date: new Date(2024, 8, 11),
-    type: FastingType.STRICT,
-  },
-  {
-    date: new Date(2024, 8, 13),
-    type: FastingType.WINE,
-    deName: "Johannes Chrysostomos",
-    enName: "John Chrysostom",
-  },
-  {
-    date: new Date(2024, 8, 14),
-    type: FastingType.WINE,
-    deName: "Kreuzerhöhung",
-    enName: "Feast of the Cross",
-  },
-  {
-    date: new Date(2024, 8, 18),
-    type: FastingType.STRICT,
-  },
-  {
-    date: new Date(2024, 8, 20),
-    type: FastingType.WINE,
-    deName: "Hl. Eustachius",
-    enName: "Saint Eustace",
-  },
-  {
-    date: new Date(2024, 8, 25),
-    type: FastingType.STRICT,
-  },
-  {
-    date: new Date(2024, 8, 27),
-    type: FastingType.STRICT,
-  },
-  {
-    date: new Date(2024, 9, 2),
-    type: FastingType.STRICT,
-  },
-  {
-    date: new Date(2024, 9, 4),
-    type: FastingType.STRICT,
-  },
-  {
-    date: new Date(2024, 9, 9),
-    type: FastingType.STRICT,
-  },
-  {
-    date: new Date(2024, 9, 11),
-    type: FastingType.STRICT,
-  },
-  {
-    date: new Date(2024, 9, 16),
-    type: FastingType.STRICT,
-  },
-  {
-    date: new Date(2024, 9, 18),
-    type: FastingType.STRICT,
-    deName: "Evangelist Lukas",
-    enName: "Luke the Evangelist",
-  },
-  {
-    date: new Date(2024, 9, 23),
-    type: FastingType.STRICT,
-  },
-  {
-    date: new Date(2024, 9, 25),
-    type: FastingType.STRICT,
-  },
-  {
-    date: new Date(2024, 9, 30),
-    type: FastingType.STRICT,
-  },
-  {
-    date: new Date(2024, 10, 6),
-    type: FastingType.STRICT,
-  },
-  {
-    date: new Date(2024, 10, 8),
-    type: FastingType.STRICT,
-  },
-  {
-    date: new Date(2024, 10, 13),
-    type: FastingType.STRICT,
-  },
-  {
-    date: new Date(2024, 10, 15),
-    type: FastingType.STRICT,
-  },
-  {
-    date: new Date(2024, 10, 16),
-    type: FastingType.FISH,
-  },
-  {
-    date: new Date(2024, 10, 17),
-    type: FastingType.FISH,
-  },
-  {
-    date: new Date(2024, 10, 18),
-    type: FastingType.FISH,
-  },
-  {
-    date: new Date(2024, 10, 19),
-    type: FastingType.FISH,
-  },
-  {
-    date: new Date(2024, 10, 20),
-    type: FastingType.STRICT,
-  },
-  {
-    date: new Date(2024, 10, 21),
-    type: FastingType.FISH,
-  },
-  {
-    date: new Date(2024, 10, 22),
-    type: FastingType.STRICT,
-  },
-  {
-    date: new Date(2024, 10, 23),
-    type: FastingType.FISH,
-  },
-  {
-    date: new Date(2024, 10, 24),
-    type: FastingType.FISH,
-  },
-  {
-    date: new Date(2024, 10, 25),
-    type: FastingType.FISH,
-  },
-  {
-    date: new Date(2024, 10, 26),
-    type: FastingType.FISH,
-  },
-  {
-    date: new Date(2024, 10, 27),
-    type: FastingType.STRICT,
-  },
-  {
-    date: new Date(2024, 10, 28),
-    type: FastingType.FISH,
-  },
-  {
-    date: new Date(2024, 10, 29),
-    type: FastingType.STRICT,
-  },
-  {
-    date: new Date(2024, 10, 30),
-    type: FastingType.FISH,
-  },
-  {
-    date: new Date(2024, 11, 1),
-    type: FastingType.FISH,
-  },
-  {
-    date: new Date(2024, 11, 2),
-    type: FastingType.FISH,
-  },
-  {
-    date: new Date(2024, 11, 3),
-    type: FastingType.FISH,
-  },
-  {
-    date: new Date(2024, 11, 4),
-    type: FastingType.WINE,
-    deName: "Barbara von Nikomedien",
-    enName: "Saint Barbara",
-  },
-  {
-    date: new Date(2024, 11, 5),
-    type: FastingType.FISH,
-  },
-  {
-    date: new Date(2024, 11, 6),
-    type: FastingType.WINE,
-    deName: "Nikolaus von Myra",
-    enName: "Saint Nicholas",
-  },
-  {
-    date: new Date(2024, 11, 7),
-    type: FastingType.FISH,
-  },
-  {
-    date: new Date(2024, 11, 8),
-    type: FastingType.FISH,
-  },
-  {
-    date: new Date(2024, 11, 9),
-    type: FastingType.FISH,
-  },
-  {
-    date: new Date(2024, 11, 10),
-    type: FastingType.FISH,
-  },
-  {
-    date: new Date(2024, 11, 11),
-    type: FastingType.STRICT,
-  },
-  {
-    date: new Date(2024, 11, 12),
+    date: new Date(2025, 2, 22),
     type: FastingType.WINE,
   },
   {
-    date: new Date(2024, 11, 13),
-    type: FastingType.STRICT,
-  },
-  {
-    date: new Date(2024, 11, 14),
-    type: FastingType.WINE,
-    deName: "Hl. Spyridon",
-    enName: "Saint Spyridon",
-  },
-  {
-    date: new Date(2024, 11, 15),
+    date: new Date(2025, 2, 23),
     type: FastingType.WINE,
   },
   {
-    date: new Date(2024, 11, 16),
+    date: new Date(2025, 2, 24),
     type: FastingType.STRICT,
   },
   {
-    date: new Date(2024, 11, 17),
+    date: new Date(2025, 2, 25),
+    type: FastingType.FISH,
+    deName: "Verkündigung des Herrn",
+    enName: "Feast of the Annunciation",
+  },
+  {
+    date: new Date(2025, 2, 26),
+    type: FastingType.STRICT,
+  },
+  {
+    date: new Date(2025, 2, 27),
+    type: FastingType.STRICT,
+  },
+  {
+    date: new Date(2025, 2, 28),
+    type: FastingType.STRICT,
+  },
+  {
+    date: new Date(2025, 2, 29),
     type: FastingType.WINE,
   },
   {
-    date: new Date(2024, 11, 18),
-    type: FastingType.STRICT,
-  },
-  {
-    date: new Date(2024, 11, 19),
-    type: FastingType.STRICT,
-  },
-  {
-    date: new Date(2024, 11, 20),
-    type: FastingType.STRICT,
-  },
-  {
-    date: new Date(2024, 11, 21),
+    date: new Date(2025, 2, 30),
     type: FastingType.WINE,
   },
   {
-    date: new Date(2024, 11, 22),
+    date: new Date(2025, 2, 31),
+    type: FastingType.STRICT,
+  },
+  {
+    date: new Date(2025, 3, 1),
+    type: FastingType.STRICT,
+  },
+  {
+    date: new Date(2025, 3, 2),
+    type: FastingType.STRICT,
+  },
+  {
+    date: new Date(2025, 3, 3),
+    type: FastingType.STRICT,
+  },
+  {
+    date: new Date(2025, 3, 4),
+    type: FastingType.STRICT,
+  },
+  {
+    date: new Date(2025, 3, 5),
     type: FastingType.WINE,
   },
   {
-    date: new Date(2024, 11, 23),
+    date: new Date(2025, 3, 6),
+    type: FastingType.WINE,
+  },
+  {
+    date: new Date(2025, 3, 7),
     type: FastingType.STRICT,
   },
   {
-    date: new Date(2024, 11, 24),
+    date: new Date(2025, 3, 8),
     type: FastingType.STRICT,
   },
-] as Array<FastingDay>;
+  {
+    date: new Date(2025, 3, 9),
+    type: FastingType.STRICT,
+  },
+  {
+    date: new Date(2025, 3, 10),
+    type: FastingType.STRICT,
+  },
+  {
+    date: new Date(2025, 3, 11),
+    type: FastingType.STRICT,
+  },
+  {
+    date: new Date(2025, 3, 12),
+    type: FastingType.WINE,
+  },
+  {
+    date: new Date(2025, 3, 13),
+    type: FastingType.FISH,
+    deName: "Palmsonntag",
+    enName: "Palm Sunday",
+  },
+  {
+    date: new Date(2025, 3, 14),
+    type: FastingType.STRICT,
+  },
+  {
+    date: new Date(2025, 3, 15),
+    type: FastingType.STRICT,
+  },
+  {
+    date: new Date(2025, 3, 16),
+    type: FastingType.STRICT,
+  },
+  {
+    date: new Date(2025, 3, 17),
+    type: FastingType.WINE,
+    deName: "Gründonnerstag",
+    enName: "Maundy Thursday",
+  },
+  {
+    date: new Date(2025, 3, 18),
+    type: FastingType.STRICT,
+  },
+  {
+    date: new Date(2025, 3, 19),
+    type: FastingType.STRICT,
+  },
+];
